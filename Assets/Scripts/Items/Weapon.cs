05b16b9a7d662ef1df829b0544328ceec81f0b57
Sakurazuka <sakurazuka90@gmail.com>
2016-02-22 22:50:30 +0100

    Weapon equip

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -9,6 +10,10 @@ public class Weapon : Item {
 
 	private int mDieceType;
 	private int mDieceNumber;
+	private Color mSelectedColor = new Color (67.0f / 255.0f, 64.0f / 255.0f, 1.0f, 1.0f); // BLUE
+	private Color mDeselectedColor = new Color (1.0f,1.0f,1.0f,100.0f/255.0f); //Default white
+
+	public static Weapon unarmed = new Weapon ("Unarmed", WeaponType.MELEE,WeaponCategory.SIMPLE,3,1,new List<EquipementTypes>());
 
 	public int DieceNumber{
 		get { return this.mDieceNumber; }
@@ -32,6 +37,18 @@ public class Weapon : Item {
 
 	public WeaponCategory Category{
 		get { return this.mCategory; }
+	}
+
+	public override void Equip(Player pmPlayer)
+	{
+		pmPlayer.equippedWeaponAttack = new Attack (mName, this);
+		PlayerSpooler.UpdateWeapon ();
+	}
+
+	public override void UnEquip(Player pmPlayer)
+	{
+		pmPlayer.equippedWeaponAttack = new Attack ("Unarmed", unarmed);
+		PlayerSpooler.UpdateWeapon ();
 	}
 
 
