66c0f1ec5ed7c7d9ee5c580827331c45552bf991
Sakurazuka <sakurazuka90@gmail.com>
2016-03-09 21:16:13 +0100

    Simple button obstacle placing

--- Assets/PlaceNewObstacle.cs
+++ Assets/PlaceNewObstacle.cs
@@ -3,5 +3,15 @@ using System.Collections;
 
 public class PlaceNewObstacle : MonoBehaviour {
 
-	
+	public GameObject obstacle;
+
+	public void Place()
+	{
+		GameObject lvObstacle = GameObject.Instantiate (obstacle);
+		FigurineStatus lvStatus = lvObstacle.GetComponent<FigurineStatus> ();
+		lvStatus.active = true;
+		lvStatus.picked = true;
+
+		GameObject.Find ("GridSelector").GetComponent<SelectFromGrid> ().creatorObstacle = lvObstacle;
+	}
 }
