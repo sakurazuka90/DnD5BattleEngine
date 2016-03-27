fbf68981289d09221905032cfd6b6baed66cf0d2
Sakurazuka <sakurazuka90@gmail.com>
2016-03-27 15:47:40 +0200

    Basic Menu

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadCreator()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void LoadGameplay()
+	{
+		SceneManager.LoadScene (2);
+	}
+
+}
