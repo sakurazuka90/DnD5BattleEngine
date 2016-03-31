f71eeaea9d2ccc0503e805f8445ba877405248ed
Sakurazuka <sakurazuka90@gmail.com>
2016-03-31 21:25:20 +0100

    Generic yes no window 2

--- Assets/BattleFieldSaver.cs
+++ Assets/BattleFieldSaver.cs
@@ -5,47 +5,70 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-public class BattleFieldSaver : MonoBehaviour {
+public class BattleFieldSaver : MonoBehaviour
+{
 
 	public GameObject assetPanel;
 	public GameObject assetStatsPanel;
 	public GameObject saveButton;
 	public GameObject saveNamePanel;
 
-	public void Save()
+	public GameObject genericYesNoWindowPrefab;
+
+	private static string _FileExistsMessage = "File with that name already exists. Do you want to save anyway?";
+
+	public void Save ()
 	{
 		GameObject lvObject = saveNamePanel.transform.FindChild ("SaveFileName").gameObject;
-		string lvFileName = lvObject.GetComponent<InputField>().text;
+		string lvFileName = lvObject.GetComponent<InputField> ().text;
 
 		if (File.Exists (Application.persistentDataPath + "/" + lvFileName + ".dat")) {
+
+			GameObject lvWindow = GameObject.Instantiate (genericYesNoWindowPrefab);
+			lvWindow.GetComponent<GenericyesNoPanelControler> ().InitializePanel (_FileExistsMessage, this.SaveMapToFile);
+
+			lvWindow.transform.parent = GameObject.Find ("Canvas").transform;
+			lvWindow.GetComponent<RectTransform> ().anchoredPosition = Vector2.zero;
+
 		} else {
-			BinaryFormatter lvFormater = new BinaryFormatter ();
-			FileStream lvFile = File.Open (Application.persistentDataPath + "/"+ lvFileName +".dat", FileMode.OpenOrCreate);
-
-			GridData lvData = new GridData ();
-			lvData.x = GridDrawer.instance.gridWidth;
-			lvData.z = GridDrawer.instance.gridHeight;
-
-			lvFormater.Serialize (lvFile, lvData);
-			lvFile.Close ();
-
+			SaveMapToFile (lvFileName);
 			Hide ();
 		}
 
 
 	}
 
-	public void Show()
+	public void SaveMapToFile ()
+	{
+		GameObject lvObject = saveNamePanel.transform.FindChild ("SaveFileName").gameObject;
+		string lvFileName = lvObject.GetComponent<InputField> ().text;
+		SaveMapToFile (lvFileName);
+	}
+
+	public void SaveMapToFile (string pmFileName)
+	{
+		BinaryFormatter lvFormater = new BinaryFormatter ();
+		FileStream lvFile = File.Open (Application.persistentDataPath + "/" + pmFileName + ".dat", FileMode.OpenOrCreate);
+
+		GridData lvData = new GridData ();
+		lvData.x = GridDrawer.instance.gridWidth;
+		lvData.z = GridDrawer.instance.gridHeight;
+
+		lvFormater.Serialize (lvFile, lvData);
+		lvFile.Close ();
+	}
+
+	public void Show ()
 	{
 		saveButton.SetActive (false);
 		assetPanel.SetActive (false);
 		assetStatsPanel.SetActive (false);
 		saveNamePanel.SetActive (true);
 		GameObject lvObject = saveNamePanel.transform.FindChild ("SaveFileName").gameObject;
-		lvObject.GetComponent<InputField>().text = "NewMap";
+		lvObject.GetComponent<InputField> ().text = "NewMap";
 	}
 
-	public void Hide()
+	public void Hide ()
 	{
 		saveButton.SetActive (true);
 		assetPanel.SetActive (true);
--- Assets/GenericyesNoPanelControler.cs
+++ Assets/GenericyesNoPanelControler.cs
@@ -9,14 +9,19 @@ public class GenericyesNoPanelControler : MonoBehaviour {
 	public GameObject noButton;
 	public GameObject textBox;
 
-	void InitializePanel(string pmMessage, UnityAction pmAction)
+	public void InitializePanel(string pmMessage, UnityAction pmAction)
 	{
+		//this.gameObject.SetActive (true);
 		yesButton.GetComponent<Button> ().onClick.AddListener (pmAction);
+		yesButton.GetComponent<Button> ().onClick.AddListener (this.Hide);
+
+		textBox.GetComponent<Text> ().text = pmMessage;
 
 	}
 
 	public void Hide()
 	{
-		
+		//this.gameObject.SetActive (false);
+		Destroy(this.gameObject);
 	}
 }
