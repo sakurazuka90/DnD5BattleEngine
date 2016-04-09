c15f20b0ceefe441b1de61dc6ab5af165d6ca546
Sakurazuka <sakurazuka90@gmail.com>
2016-04-09 17:49:16 +0200

    Rotation of obstacle

--- Assets/ObstacleRotator.cs
+++ Assets/ObstacleRotator.cs
@@ -6,7 +6,7 @@ public class ObstacleRotator : MonoBehaviour {
 	public void Rotate(float pmAngle)
 	{
 		foreach (Transform lvChild in this.transform) {
-			if (lvChild.CompareTag ("ObstacleMesh"))
+			if (lvChild.CompareTag ("ObstacleRoot"))
 				lvChild.transform.Rotate (new Vector3 (0.0f, pmAngle, 0.0f));
 		}
 			
