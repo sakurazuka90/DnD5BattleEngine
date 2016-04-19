ae83229a2314c74b2898d6dee8051e12969700ab
Sakurazuka <sakurazuka90@gmail.com>
2016-04-19 17:07:13 +0200

    Resize of panels, functional panel

--- Assets/PanelShrinker.cs
+++ Assets/PanelShrinker.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class PanelShrinker : MonoBehaviour {
 
 	List<GameObject> _childrenObjects;
 	private bool isShrank = false;
+	private float _savedHeight; 
+	private RectTransform _transform;
 
 	// Use this for initialization
 	void Start () {
 		_childrenObjects = new List<GameObject> ();
+		_transform = this.gameObject.GetComponent<RectTransform> ();
+		_savedHeight = _transform.sizeDelta.y;
 	}
 	
 	// Update is called once per frame
@@ -24,6 +29,14 @@ public class PanelShrinker : MonoBehaviour {
 
 		isShrank = !isShrank;
 
+		float lvHeightTemp;
 
+		if (_transform.sizeDelta.y > 0.0f) {
+			lvHeightTemp = 0.0f;// - _transform.sizeDelta.y;
+		} else {
+			lvHeightTemp = /*_transform.sizeDelta.y - */_savedHeight;
+		}
+
+		_transform.sizeDelta = new Vector2 (_transform.sizeDelta.x,lvHeightTemp);
 	}
 }
