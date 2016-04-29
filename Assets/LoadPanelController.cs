e6262920745720ee962b71800f560249f967b28f
sakurazuka <sakurazuka90@gmail.com>
2016-04-29 17:17:13 +0200

    Move Common LoadPanel code to abstract class

--- Assets/LoadPanelController.cs
+++ Assets/LoadPanelController.cs
@@ -4,57 +4,14 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
-public class LoadPanelController : MonoBehaviour {
+public class LoadPanelController : AbstractPanelController {
 
-	public GameObject content;
-	public GameObject optionPrefab;
-
-	private string _selected;
-
-
-	// Use this for initialization
-	void Start () {
-		GenerateContent ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	public void GenerateContent()
+	protected override List<string> GetOptions ()
 	{
-		List<string> lvNames = BattlefieldStateReader.instance.ListFiles ();
-
-
-
-		content.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0, lvNames.Count * 25);
-
-		foreach (string lvName in lvNames) {
-			GameObject lvInstance = Instantiate (optionPrefab);
-			lvInstance.transform.parent = content.transform;
-			lvInstance.GetComponent<SelectOptionController> ().SetName (lvName);
-		}
+		return BattlefieldStateReader.instance.ListFiles ();
 	}
 
-	public void Select(string pmFilename)
-	{
-		_selected = pmFilename;
-		GameObject.Find ("LoadBattlefieldButton").GetComponent<Button>().interactable = true;
-		GameObject.Find ("DeleteFileButton").GetComponent<Button>().interactable = true;
-	}
-
-	public void DeselectAll()
-	{
-		foreach (Transform lvOption in content.transform) {
-			lvOption.gameObject.GetComponent<SelectOptionController> ().SetBackgroundColor (new Color (255.0F / 255.0F, 255.0F / 255.0F, 255.0F / 255.0F));
-			lvOption.gameObject.GetComponent<SelectOptionController> ().SetTextColor (new Color (50.0F / 255.0F, 50.0F / 255.0F, 50.0F / 255.0F));
-			this._selected = null;
-		}
-			
-	}
-
-	public void Load()
+	public override void Load()
 	{
 		BattlefieldConstructor.instance.GenerateGameplay(_selected);
 
--- Assets/Scripts/GUI/SelectPanel/AbstractPanelController.cs
+++ Assets/Scripts/GUI/SelectPanel/AbstractPanelController.cs
@@ -1,10 +1,62 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public abstract class AbstractPanelController
+public abstract class AbstractPanelController : MonoBehaviour
 {
+	public GameObject content;
+	public GameObject optionPrefab;
+
+	public GameObject [] buttons;
+
+	protected string _selected;
+
+	// Use this for initialization
+	void Start () {
+		GenerateContent ();
+	}
+
+
 	public AbstractPanelController ()
 	{
 	}
+
+	public virtual void GenerateContent()
+	{
+		List<string> lvNames = GetOptions();
+
+		content.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0, lvNames.Count * 25);
+
+		foreach (string lvName in lvNames) {
+			GameObject lvInstance = Instantiate (optionPrefab);
+			lvInstance.transform.parent = content.transform;
+			lvInstance.GetComponent<SelectOptionController> ().SetName (lvName);
+		}
+	}
+
+	protected abstract List<string> GetOptions();
+
+	public virtual void Select(string pmFilename)
+	{
+		_selected = pmFilename;
+
+		foreach (GameObject button in buttons) {
+			button.GetComponent<Button>().interactable = true;
+		}
+	}
+
+	public virtual void DeselectAll()
+	{
+		foreach (Transform lvOption in content.transform) {
+			lvOption.gameObject.GetComponent<SelectOptionController> ().SetBackgroundColor (new Color (255.0F / 255.0F, 255.0F / 255.0F, 255.0F / 255.0F));
+			lvOption.gameObject.GetComponent<SelectOptionController> ().SetTextColor (new Color (50.0F / 255.0F, 50.0F / 255.0F, 50.0F / 255.0F));
+			this._selected = null;
+		}
+
+	}
+
+	public abstract void Load();
 
 
 }
