0104fe154aaec818d1bfa9911dbfae3a8ca26370
sakurazuka <sakurazuka90@gmail.com>
2016-05-01 21:53:33 +0200

    Select figurine

---
+++ Assets/FigurineFileReader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FigurineFileReader : MonoBehaviour {
+
+	private static string FIGURINE_LIBRARY_NAME = "figurineLibrary.flib";
+
+	public static FigurineFileReader instance;
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (this.gameObject);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public List<string> GetFigurineList()
+	{
+		List<string> lvNames = new List<string> ();
+
+		lvNames.Add ("DYNAMIC SPAWN POINT");
+
+		lvNames.Add ("Goblin");
+		lvNames.Add ("Dwarf");
+
+		return lvNames;
+	}
+}
---
+++ Assets/Scripts/GUI/SelectPanel/SelectFigurineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SelectFigurineController : AbstractPanelController
+{
+	public SelectFigurineController ()
+	{
+	}
+
+	#region implemented abstract members of AbstractPanelController
+
+	protected override List<string> GetOptions ()
+	{
+		return FigurineFileReader.instance.GetFigurineList ();
+	}
+
+	public override void Load ()
+	{
+		throw new NotImplementedException ();
+	}
+
+	#endregion
+}
+
