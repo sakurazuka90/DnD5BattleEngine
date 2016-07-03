0d591ea42025afec096bec114e1d7e71c354902e
Sakurazuka <sakurazuka90@gmail.com>
2016-07-03 10:55:59 +0200

    Panel Selector

---
+++ Assets/PanelSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelSelector : MonoBehaviour {
+
+	public GameObject[] panels;
+
+	public void ShowPanel(int pmIndex)
+	{
+		foreach (GameObject panel in panels) {
+			panel.SetActive (false);
+		}
+
+		panels [pmIndex].SetActive (true);
+	}
+}
