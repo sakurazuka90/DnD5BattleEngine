aa9a22238f937f1d05d24c54fc0dfd53c050f264
sakustation\Sakurazuka <Sakurazuka90@gmail.com>
2016-10-14 21:48:59 +0200

    Blocks

---
+++ Assets/ObstacleBaseMapper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleBaseMapper : MonoBehaviour {
+
+	public GameObject block;
+	public bool[] mapping = new bool[9];
+
+	// Use this for initialization
+	void Start () {
+		mapping [4] = true;
+		PlaceBlocks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void PlaceBlocks()
+	{
+		float zMove = -1.0F;
+
+		int level = 0;
+
+		for (int j = 0; j < 3; j++) {
+			float xMove = -1.0F;
+			for (int i = 0; i < 3; i++) {
+				if (mapping [i + (level * 3)]) {
+					GameObject instance = Instantiate (block);
+					instance.transform.SetParent (this.gameObject.transform);
+					instance.transform.Translate (new Vector3 (xMove, 1.0f, zMove));
+				}
+				xMove += 1.0F;
+			}
+			zMove += 1.0F;
+			level++;
+		}
+	}
+}
